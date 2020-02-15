3840dbf94e5af30959d4171521326833d4ff29e7
xuweijie <xuweijie@didi.com>
2020-02-15 14:46:42 +0800

    多线程消费

--- src/main/java/com/jay/kafka/chapter3/FirstMultiConsumerThreadDemo.java
+++ src/main/java/com/jay/kafka/chapter3/FirstMultiConsumerThreadDemo.java
@@ -1,7 +1,69 @@
 package com.jay.kafka.chapter3;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 /**
+ * 多线程消费实现一
+ *
  * @author xuweijie
  */
 public class FirstMultiConsumerThreadDemo {
+
+    private static final String BROKER_LIST = "localhost:9092";
+    private static final String TOPIC = "topic-demo-2";
+    private static final String GROUP = "group.demo";
+
+    private static Properties initConfig() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_LIST);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP);
+        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer.client.id.demo");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        return properties;
+    }
+
+    public static void main(String[] args) {
+        Properties props = initConfig();
+        int consumerThreadNum = 4;
+        for (int i = 0; i < consumerThreadNum; i++) {
+            new KafkaConsumerThread(props, TOPIC).start();
+        }
+
+    }
+
+    private static class KafkaConsumerThread extends Thread {
+        private KafkaConsumer<String, String> kafkaConsumer;
+
+        KafkaConsumerThread(Properties props, String topic) {
+            this.kafkaConsumer = new KafkaConsumer<>(props);
+            this.kafkaConsumer.subscribe(Arrays.asList(topic));
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    ConsumerRecords<String, String> records = this.kafkaConsumer.poll(Duration.ofMillis(1000));
+                    for (ConsumerRecord<String, String> record : records) {
+                        // 处理消息模块
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                this.kafkaConsumer.close();
+            }
+        }
+    }
+
 }
--- src/main/java/com/jay/kafka/chapter3/ThirdMultiConsumerThreadDemo.java
+++ src/main/java/com/jay/kafka/chapter3/ThirdMultiConsumerThreadDemo.java
@@ -9,20 +9,21 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
- * 多线程消费实现一
+ * 多线程消费实现三
  *
  * @author xuweijie
  */
-public class FirstMultiConsumerThreadDemo {
-
+public class ThirdMultiConsumerThreadDemo {
 
     private static final String BROKER_LIST = "localhost:9092";
     private static final String TOPIC = "topic-demo-2";
     private static final String GROUP = "group.demo";
-    private static AtomicBoolean isRunning = new AtomicBoolean(true);
 
     private static Properties initConfig() {
         Properties properties = new Properties();
@@ -31,24 +32,26 @@ public class FirstMultiConsumerThreadDemo {
         properties.put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer.client.id.demo");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         return properties;
     }
 
     public static void main(String[] args) {
         Properties props = initConfig();
-        int consumerThreadNum = 4;
-        for (int i = 0; i < consumerThreadNum; i++) {
-            new KafkaConsumerThread(props, TOPIC).start();
-        }
-
+        new KafkaConsumerThread(props, TOPIC, Runtime.getRuntime().availableProcessors()).start();
     }
 
     private static class KafkaConsumerThread extends Thread {
         private KafkaConsumer<String, String> kafkaConsumer;
+        private ExecutorService executorService;
+        private int threadNumber;
 
-        public KafkaConsumerThread(Properties props, String topic) {
-            this.kafkaConsumer = new KafkaConsumer<>(props);
-            this.kafkaConsumer.subscribe(Arrays.asList(topic));
+        private KafkaConsumerThread(Properties props, String topic, int threadNumber) {
+            kafkaConsumer = new KafkaConsumer<>(props);
+            kafkaConsumer.subscribe(Arrays.asList(topic));
+            this.threadNumber = threadNumber;
+            executorService = new ThreadPoolExecutor(threadNumber, threadNumber, 0L, TimeUnit.MILLISECONDS,
+                    new ArrayBlockingQueue<>(1000), new ThreadPoolExecutor.CallerRunsPolicy());
         }
 
         @Override
@@ -56,14 +59,30 @@ public class FirstMultiConsumerThreadDemo {
             try {
                 while (true) {
                     ConsumerRecords<String, String> records = this.kafkaConsumer.poll(Duration.ofMillis(1000));
-                    for (ConsumerRecord<String, String> record : records) {
-                        // 处理消息模块
+                    if (!records.isEmpty()) {
+                        executorService.submit(new RecordsHandler(records));
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 this.kafkaConsumer.close();
+            }
+        }
+    }
+
+    private static class RecordsHandler extends Thread {
+        private final ConsumerRecords<String, String> records;
+
+        private RecordsHandler(ConsumerRecords<String, String> records) {
+            this.records = records;
+        }
+
+        @Override
+        public void run() {
+            // 处理消息
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.value());
             }
         }
     }
